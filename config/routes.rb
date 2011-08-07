885c7ccae88dd7ffc9b94b329343a8f6299cebee
vanilla <hxy229@gmail.com>
2011-08-07 08:22:43 -0600

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Chp3::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/about"
 
   get "pages/contact"
+  
+  match '/signup', :to => 'users#new'
+  
+  match '/contact', :to => 'pages#contact'
+  
+  match '/about', :to => 'pages#about'
+  
+  match '/help', :to => 'pages#help'
+  
+  root :to => 'pages#home'
+  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
